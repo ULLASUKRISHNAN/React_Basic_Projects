4f029c7bfcc48622aeff07b98ac4ec87fa66580d
UllasUKrishnan <krishnanullas3@gmail.com>
2021-12-09 13:47:27 +0530

    Menu plans created

---
+++ basic-projects/Menus/src/App.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import Menu from './Menu';
+import Categories from './Categories';
+import items from './data';
+
+//fetching the unique category
+const getCategories = ['all', ...new Set(items.map((item) => item.category))];
+
+function App() {
+  const [menu, setMenu] = useState(items);
+  const [categories, setCategories] = useState(getCategories);
+  // filter the menu based on category
+  const filterMenu = (category) => {
+    if (category === 'all') {
+      setMenu(items);
+      return;
+    }
+
+    //Update the Categories
+    const newCategories = items.filter((item) => item.category === category);
+    setMenu(newCategories);
+  };
+  return (
+    <main>
+      <section className='menu section'>
+        <div className='title'>
+          <h2>Menu Plans</h2>
+          <div className='underline'></div>
+        </div>
+        <Categories filterMenu={filterMenu} categories={categories} />
+        <Menu menu={menu} />
+      </section>
+    </main>
+  );
+}
+
+export default App;
---
+++ basic-projects/Menus/src/Categories.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Categories = ({ filterMenu, categories }) => {
+  return (
+    <div className='btn-container'>
+      {categories.map((category, index) => {
+        return (
+          <button
+            key={index}
+            type='button'
+            className='filter-btn'
+            onClick={() => filterMenu(category)}
+          >
+            {category}
+          </button>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Categories;
